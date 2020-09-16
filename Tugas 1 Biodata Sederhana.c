8dfb7ab1254400b8993cb853de98f8bcc1f0b6ea
adityaprimayudha <adityaprimayudha91@gmail.com>
2020-09-16 11:43:16 +0700

    Biodata Saya

---
+++ Tugas 1 Biodata Sederhana.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	printf("================== Biodata Diri ====================\n\n");
+	printf("Nama\t\t: Aditya Primayudha\n");
+	printf("NPM\t\t: 19081010104\n");
+	printf("Alamat\t\t: Kebumen, Jawa Tengah\n");
+	printf("Jenis Kelamin\t: Laki-laki\n");
+	printf("Tempat Lahir\t: Palembang, Sumatera Selatan\n");
+	printf("Tanggal Lahir\t: 26 Maret 2001\n");
+	printf("Pekerjaan\t: Mahasiswa\n");
+	printf("Email\t\t: adityaprimayudha91@gmail.com\n\n");
+	printf("====================== Sekian =======================");
+	return 0;
+}
